8bbb90057a862d69175fa88ff19a92b27b3b162f
Jingyi Li <noon@berkeley.edu>
2016-11-09 12:10:53 -0800

    edit script for recording

--- detecting.py
+++ detecting.py
@@ -1,12 +1,26 @@
 from time import sleep
+import os 
+import signal
+import subprocess
+import CHIP_IO.GPIO as GPIO
+import datetime
 
 # adding an event trigger on the rising edge
 
-import CHIP_IO.GPIO as GPIO
+is_playing = False 
+global musicprocess
+
 GPIO.setup("XIO-P0", GPIO.IN)
 GPIO.add_event_detect("XIO-P0", GPIO.RISING)
 if GPIO.event_detected("XIO-P0"):
     print "Rising edge detected ==========="
+    if not is_playing:
+    	fname = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    	musicprocess = subprocess.Popen(['arecord', '-f', 'cd', '-D', 'hw:0,0', fname], stdout=PIPE, stderr=PIPE)
+    	is_playing = True 
+    else: 
+    	os.killpg(os.getpdig(musicprocess.pid), signal.SIGTERM)
+    	is_playing = False 
 
 # constantly pump out the current state.
 
