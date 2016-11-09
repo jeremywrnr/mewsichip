646107fd96beb5b42ddb1b6b712f2b11ebd3c9f9
Jeremy Warner <jeremywrnr@gmail.com>
2016-11-08 22:15:40 -0800

    pr3event exit

--- detecting.py
+++ detecting.py
@@ -6,10 +6,13 @@ import CHIP_IO.GPIO as GPIO
 GPIO.setup("XIO-P0", GPIO.IN)
 GPIO.add_event_detect("XIO-P0", GPIO.RISING)
 if GPIO.event_detected("XIO-P0"):
-    print "Rising edge detected ==========="
+    print "Button press detected ==========="
+
+def loop():
+    return 0
 
 while True:
-    # prevent exiting
+    loop() # prevent exiting
 
 # constantly pump out the current state.
 # if GPIO.input("XIO-P0"):
