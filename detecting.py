94831d465e0c58e7467c11c76bf9cb7f1def3db3
Jeremy Warner <jeremywrnr@gmail.com>
2016-11-08 22:36:22 -0800

    wow, trump/america. wow

--- detecting.py
+++ detecting.py
@@ -1,6 +1,7 @@
 # adding an event trigger on the rising edge
 
 import CHIP_IO.GPIO as GPIO
+from time import sleep
 
 channel = "XIO-P0"
 record = False
@@ -9,9 +10,13 @@ GPIO.setup(channel, GPIO.IN)
 print "Mewsician starting."
 
 while True: # continually in this state
-    GPIO.wait_for_edge(channel, GPIO.RISING)
+
+    GPIO.wait_for_edge(channel, GPIO.FALLING)
     print "Button press detected =========="
-    if record == GPIO.input(channel):
+
+    sleep(0.1) # wait for debouncing
+
+    if record:
         print "Stopping recording."
         record = False
     else:
