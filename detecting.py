c03a4cb12217fb17ee0aa990957b0f666824219e
Jeremy Warner <jeremywrnr@gmail.com>
2016-11-08 22:15:40 -0800

    pr3event exit

--- detecting.py
+++ detecting.py
@@ -1,5 +1,5 @@
 from time import sleep
-import os 
+import os
 import signal
 import subprocess
 import CHIP_IO.GPIO as GPIO
@@ -7,7 +7,7 @@ import datetime
 
 # adding an event trigger on the rising edge
 
-is_playing = False 
+is_playing = False
 global musicprocess
 
 GPIO.setup("XIO-P0", GPIO.IN)
@@ -15,15 +15,15 @@ GPIO.add_event_detect("XIO-P0", GPIO.RISING)
 if GPIO.event_detected("XIO-P0"):
     print "Rising edge detected ==========="
     if not is_playing:
-    	fname = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    	musicprocess = subprocess.Popen(['arecord', '-f', 'cd', '-D', 'hw:0,0', fname], stdout=PIPE, stderr=PIPE)
-    	is_playing = True 
-    else: 
-    	os.killpg(os.getpdig(musicprocess.pid), signal.SIGTERM)
-    	is_playing = False 
+        fname = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        musicprocess = subprocess.Popen(['arecord', '-f', 'cd', '-D', 'hw:0,0', fname], stdout=PIPE, stderr=PIPE)
+        is_playing = True
+    else:
+        os.killpg(os.getpdig(musicprocess.pid), signal.SIGTERM)
+        is_playing = False
 
 while True:
-    # prevent exiting
+    loop() # prevent exiting
 
 # constantly pump out the current state.
 # if GPIO.input("XIO-P0"):
