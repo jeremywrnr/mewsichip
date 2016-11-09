4b95e071d45599f083d7110ef4b57b4578ca0fca
Jeremy Warner <jeremywrnr@gmail.com>
2016-11-08 22:44:23 -0800

    well then

--- detecting.py
+++ detecting.py
@@ -3,11 +3,24 @@
 import CHIP_IO.GPIO as GPIO
 from time import sleep
 
+# include later:
+# - meteor userId
+# - authentication
 channel = "XIO-P0"
-record = False
+recording = False
 
 GPIO.setup(channel, GPIO.IN)
 print "Mewsician starting."
+
+def record():
+    print "Starting recording."
+    # trigger external recording
+    # combine current time and uid
+
+def upload():
+    print "Stopping recording."
+    # trigger external uploading
+    # will use userId and auth
 
 while True: # continually in this state
 
@@ -15,13 +28,13 @@ while True: # continually in this state
     GPIO.wait_for_edge(channel, GPIO.FALLING)
     print "Button press detected =========="
 
-    sleep(3) # wait for debouncing 3 secs
-
-    if record:
-        print "Stopping recording."
-        record = False
+    if recording:
+        upload()
+        recording = False
     else:
-        print "Starting recording."
-        record = True
+        recordi()
+        recording = True
+
+    sleep(3) # wait for debouncing 3 secs, bad.
 
 GPIO.cleanup()
