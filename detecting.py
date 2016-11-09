9daec134f34f7262c191a3ad9d18ea638236f419
Jeremy Warner <jeremywrnr@gmail.com>
2016-11-08 21:58:03 -0800

    from time to import sleep

--- detecting.py
+++ detecting.py
@@ -1,8 +1,9 @@
-import CHIP_IO.GPIO as GPIO
-GPIO.setup("XIO-P0", GPIO.IN)
+from time import sleep
 
 # adding an event trigger on the rising edge
 
+import CHIP_IO.GPIO as GPIO
+GPIO.setup("XIO-P0", GPIO.IN)
 GPIO.add_event_detect("XIO-P0", GPIO.RISING)
 if GPIO.event_detected("XIO-P0"):
     print "Rising edge detected ==========="
