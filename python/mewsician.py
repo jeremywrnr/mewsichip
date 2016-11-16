481ad8fea18f1e3dde4567c656ed21f3a9af05c1
Jeremy Warner <jeremywrnr@gmail.com>
2016-11-16 00:33:55 -0800

    merge python

---
+++ python/mewsician.py
@@ -0,0 +1,50 @@
+import CHIP_IO.GPIO as GPIO
+from time import sleep
+import subprocess
+import datetime
+import signal
+import os
+
+
+# TODO source authentication from device environment file
+# TODO mechanism for creation / uploading of these??????
+GPIO.cleanup()
+output = "XIO-P1"
+channel = "XIO-P0"
+GPIO.setup(output, GPIO.OUT)
+GPIO.setup(channel, GPIO.IN)
+GPIO.add_event_detect(channel, GPIO.RISING)
+print "Mewsician starting."
+
+global mpid
+def record():
+    # trigger external recording and create a new subprocess for this
+    print "Starting recording." # combine current time and uid
+    fname = datetime.datetime.now().strftime("%Y-%m-%d@%H-%M-%S") + '.mp3'
+    cmd = "arecord -f cd -D hw:0,0 -t raw | lame -x -r - - > " + fname
+    mpid = subprocess.Popen(['arecord', '-f', 'cd', '-D', 'hw:0,0', fname], stdout=PIPE, stderr=PIPE)
+    print 'musicprocess pid is ', musicprocess.pid
+    return (musicprocess.pid,  fname)
+
+def upload(mpid):
+    # trigger external uploading
+    # will use userId and auth
+    print "Stopping recording."
+    print 'stop: musicprocess pid is ', mpid
+    # os.killpg(os.getpgid(mpid), signal.SIGTERM)
+
+global recording
+def trigger():
+    if recording:
+        upload(mpid)
+        recording = False
+    else:
+        mpid = record()
+        recording = True
+        sleep(3) # wait for debouncing 3 secs, bad.
+
+while True: # continually in this state
+    if GPIO.event_detected(channel):
+        print "MEOW! =＾ᵒ⋏ᵒ＾= "
+        print "edge detected."
+        trigger()
--- script/unexport.sh
+++ script/unexport.sh
@@ -2,6 +2,6 @@
 
 for F in `ls -1d /sys/class/gpio/gpio*`; do :
   GPIO=`echo $F | sed 's/^[^0-9]*//'`
-  echo $F $GPIO
+  echo "$F // $GPIO"
   echo $GPIO >/sys/class/gpio/unexport
 done
