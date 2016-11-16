e4f3e0bb881f43ffc69f7c0c143ecd95c02e216f
Jeremy Warner <jeremywrnr@gmail.com>
2016-11-16 00:14:57 -0800

    fix unexport

--- script/unexport.sh
+++ script/unexport.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for F in /sys/class/gpio/gpio[0-9]*; do :
+for F in `ls -1d /sys/class/gpio/gpio*`; do :
   GPIO=`echo $F | sed 's/^[^0-9]*//'`
   echo $F $GPIO
   echo $GPIO >/sys/class/gpio/unexport
