79ab27942af39034f8c0326cafa0c1fb846114ab
Jeremy Warner <jeremywrnr@gmail.com>
2016-11-16 06:04:30 -0800

    quiet start and fix file cleanup

--- script/unexport.sh
+++ script/unexport.sh
@@ -2,6 +2,6 @@
 
 for F in `ls -1d /sys/class/gpio/gpio*`; do :
   GPIO=`echo $F | sed 's/^[^0-9]*//'`
-  echo "$F // $GPIO"
+  # echo "$F || $GPIO" # debugging
   echo $GPIO >/sys/class/gpio/unexport 2>/dev/null
 done
