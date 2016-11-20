8e1402a2e623978f88bc79ed2548f313fd9f2613
Jeremy Warner <jeremywrnr@gmail.com>
2016-11-20 12:50:12 -0800

    rough framework

--- python/mewsicode.py
+++ python/mewsicode.py
@@ -16,7 +16,11 @@ GPIO.setup(outled, GPIO.OUT)
 GPIO.output(outled, GPIO.HIGH)
 GPIO.add_event_detect(channel, GPIO.RISING)
 
-print "\n<||||| - mewsician . commences - |||||>\n"
+
+# hardcoded file locations
+codeLoc = "/home/chip/mewsician/"
+audioLoc = "/home/chip/audio/"
+queue = audioLoc + "queue"
 
 # auth passed in from (../start)
 authentication = sys.argv[1]
@@ -24,6 +28,19 @@ recording = False
 fname = None
 bname = None
 mpid = None
+
+
+# code that runs once when the script is starting.
+def startup():
+    print "\n<||||| - mewsician . commences - |||||>\n"
+    try: # reupload and files that previously failed
+        for file in open(queue, 'r'):
+            retryUpload(file)
+    except: # no uploads || !file exists yet || upload err
+        args = ['touch', queue]
+        subprocess.Popen(args)
+        print(e)
+
 
 # trigger external recording and create a new subprocess for this
 # combine current time and uid for the filename to be uploaded
@@ -47,10 +64,16 @@ def record():
 '---''(_/--'  `-'\_)
         """) # =^_^=
 
-# stop current recording
-# trigger external uploading
-# will use authentication
+
+def retryUpload(file):
+    # TODO implement
+    print(file)
+
+
+# stop current recording, compress file format
+# trigger external uploading if connected to the network
 def upload():
+    global mname
     GPIO.output(outled, GPIO.HIGH)
     print("\nStopping recording...")
     mpid.terminate() # from record()
@@ -65,27 +88,32 @@ def upload():
     auth = 'auth=' + authentication # upload
     path =  os.getcwd() + '/' + mname
     upload = 'file=@' + path
-    args = ['curl', '--form', upload, '--form', auth, 'http://mewsician.win/upload']
-    print(subprocess.check_output(args))
 
-    print("Cleaning up...")
-    # doesn't seem to recognize the home environment
-    # subprocess.call(['mv', '-v', mname, os.environ['HOME'] + "/audio/"])
-    subprocess.call(['mv', '-v', mname, "/home/chip/audio/"])
-    subprocess.call(['rm', '-v', fname])
-    print("Complete.")
+    args = ['curl', '--form', upload, '--form', auth, 'http://mewsician.win/upload']
+    try: # if upload fails (no network connection), write it to a queue
+        print(subprocess.check_output(args))
+        print("Cleaning up...")
+        subprocess.call(['mv', '-v', mname, audioLoc])
+        subprocess.call(['rm', '-v', fname])
+        print("Complete.")
+    except subprocess.CalledProcessError as e:
+        with open(audioLoc + "queue", "a") as q:
+            q.write(mname)
+        print(e.output)
+
 
 def trigger():
+    global recording
     if recording:
         upload()
-        global recording
         recording = False
     else: # start recording
         record()
-        global recording
         recording = True
     sleep(3) # wait 3 secs for debouncing, bad but works.
+
 
 while True: # continually in this state, check if channel HI
     if GPIO.event_detected(channel) and GPIO.input(channel):
         trigger() # on button press, trigger callback
+
