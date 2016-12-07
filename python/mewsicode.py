56e677d80c2737d65e2616fa7c02ff207dbfd7bd
Sarah Sterman <ssterman@berkeley.edu>
2016-12-06 23:38:41 -0800

    no playable music check

--- python/mewsicode.py
+++ python/mewsicode.py
@@ -50,14 +50,15 @@ def playback():
 
     # get a random audio clip to play from the audio folder
     music_files = [f for f in os.listdir(audioLoc) if f.endswith('.mp3')]
-    print len(music_files)
-    music_path = audioLoc + music_files[random.randint(0, len(music_files))]
-    print music_path
-    playback_fname = music_path
+    if len(music_files) is not 0:
+        music_path = audioLoc + music_files[random.randint(0, len(music_files))]
+        playback_fname = music_path
 
-    args = ['mpg123', playback_fname]
-    playproc = subprocess.Popen(args)
-    play_pid = psutil.Process(playproc.pid)
+        args = ['mpg123', playback_fname]
+        playproc = subprocess.Popen(args)
+        play_pid = psutil.Process(playproc.pid)
+    else:
+        print 'no playable music...'
 
 
 # code that runs once when the script is starting.
